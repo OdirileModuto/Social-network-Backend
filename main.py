e0f0120843ac2560950ba7942d782777750e9785
OdirileModuto <138676420+OdirileModuto@users.noreply.github.com>
2024-10-24 10:39:30 +0200

    Social

--- main.py
+++ main.py
@@ -1,108 +1,115 @@
 from sqlalchemy import create_engine, ForeignKey, String, Integer, Column
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import String
 from sqlalchemy.orm import sessionmaker
 import uuid
+import os
+import logging
+from dotenv import load_dotenv
+
+
+load_dotenv(".env", verbose=True)
+
+db_user = os.environ['PGUSER']
+db_password = os.environ['PGPASSWORD']
+db_host = os.environ['PGHOST']
+db_port = os.environ['PGPORT']
+db_name = os.environ['PGDATABASE']
+url = f'postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}'
+
+logging.basicConfig(
+    level=logging.INFO, 
+    format='%(asctime)s - %(levelname)s - %(message)s'  
+)
 
 Base = declarative_base()
-
 
 def generate_uuid():
     return str(uuid.uuid4())
 
-class users (Base):
+class users(Base):
     __tablename__ = "users"
-    userID = Column("userID",String, primary_key= True, default= generate_uuid)
-    firstName= Column("firstName", String)
-    lastName= Column("lastName",String)
-    profileName= Column("profileName",String)
-    email = Column ("email", String)  
+    userID = Column(String, primary_key=True, default=generate_uuid)
+    firstName = Column(String)
+    lastName = Column(String)
+    profileName = Column(String)
+    email = Column(String)
 
-
-    def __init__ ( self,firstName,lastName,profileName,email):
+    def __init__(self, firstName, lastName, profileName, email):
         self.firstName = firstName
         self.lastName = lastName
         self.profileName = profileName
-        self.email = email 
+        self.email = email
 
-class post (Base):
+class post(Base):
     __tablename__ = "post"
-    postId = Column ("postId", String, primary_key=True, default= generate_uuid)
-    userId = Column ("userId", String, ForeignKey("users.userID"))
-    postContent = Column ("postContent", String)
+    postId = Column(String, primary_key=True, default=generate_uuid)
+    userId = Column(String, ForeignKey("users.userID"))
+    postContent = Column(String)
 
-    def __init__(self,userId, postContent):
+    def __init__(self, userId, postContent):
         self.userId = userId
         self.postContent = postContent
 
-class likes (Base):
+class likes(Base):
     __tablename__ = "likes"
-    likeId = Column("likeId",String, primary_key= True, default= generate_uuid)
-    userId = Column("userId", String, ForeignKey ("users.userID"))
-    postId = Column("postId", String, ForeignKey ("post.postId"))
+    likeId = Column(String, primary_key=True, default=generate_uuid)
+    userId = Column(String, ForeignKey("users.userID"))
+    postId = Column(String, ForeignKey("post.postId"))
 
-    def __init__ (self,userId,postId):
+    def __init__(self, userId, postId):
         self.userId = userId
         self.postId = postId
- 
 
-def addUser(firstName, lastName, profileName, email,session): 
-
-    exist = session.query(users).filter(users.email==email).all()
-    if len(exist)>0:
-        print("Email Address already exist")
+def addUser(firstName, lastName, profileName, email, session):
+    exist = session.query(users).filter(users.email == email).all()
+    if len(exist) > 0:
+        print("Email Address already exists")
     else:
-        user = users(firstName,lastName,profileName,email)
+        user = users(firstName, lastName, profileName, email)
         session.add(user)
         session.commit()
-    print ("User added to database")
+    logging.info("User added to database")
 
-
-def addPost( userId,postContent,session):
-    newPost = post (userId,postContent)  
+def addPost(userId, postContent, session):
+    newPost = post(userId, postContent)
     session.add(newPost)
     session.commit()
 
-
-def addLike(userId, postId):
-    like= likes(userId,postId)
+def addLike(userId, postId, session):  # Added session argument here
+    like = likes(userId, postId)
     session.add(like)
     session.commit()
-    print("like was added")
+    logging.info("Like was added")
 
-    
-            
-
-db = "sqlite:///socialDB.db"   
-engine = create_engine(db)
+# Connect to the PostgreSQL database
+engine = create_engine(url)
 Base.metadata.create_all(bind=engine)
-
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+firstName = "Kyle"
+lastName = "Jackson"
+profileName = "kayJ86"
+email = "kyle@gmail.com"
+addUser(firstName, lastName, profileName, email, session)
 
-firstName = " Clark"
-lastName = "Histon"
-profileName = "Clark8741"
-email = "Clark94@gmail.com"
-addUser(firstName,lastName,profileName,email,session)
+userId = "270788d2-6a1a-496a-a845-7e26e16c64aa"
+postId = "c1a4292a-c51d-4aa4-8d2c-500aee3cb933"
+postContent = "Summer Season is upon us"
+addPost(userId, postContent, session)
 
-#  create a post    
-userId ="1aedd157-f782-4159-a64a-d36779923416"
-postId = "0b699f3c-a960-41f6-b405-11164eaae000"
-postContent ="Finaly My code is Working"
-addPost(userId,postContent,session)
+# Retrieve and print all posts for the user
 allPost = session.query(post).filter(post.userId == userId).all()
-userPost = []
-for p in allPost:
-    userPost.append(p.postContent)
-    print(userPost)
-addLike(userId, postId)
-postLikes = session.query(likes).filter(likes.postId==postId).all()
+userPost = [p.postContent for p in allPost]
+print(userPost)
+
+# Add a like and check post likes
+addLike(userId, postId, session)
+postLikes = session.query(likes).filter(likes.postId == postId).all()
 print(len(postLikes))
 
-usersLikedPost = session.query(users,likes).filter(likes.postId==postId).filter(likes.userId==users.userID)
+# Retrieve users who liked the post
+usersLikedPost = session.query(users, likes).filter(likes.postId == postId).filter(likes.userId == users.userID)
 for u, l in usersLikedPost:
-    print(u.firstName,u.lastName)
-
+    logging.info(f'{u.firstName} {u.lastName}')  # Proper logging format
